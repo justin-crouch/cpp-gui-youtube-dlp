4e67e387657568cfc2a8b7c731bb5a25dba99877
Justin Crouch <jcrouch938@gmail.com>
2024-04-27 16:20:45 +0200

    Add project files.

---
+++ youtube-dl/youtube-dl.cpp
@@ -0,0 +1,12 @@
+﻿// youtube-dl.cpp : Defines the entry point for the application.
+//
+
+#include "youtube-dl.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
